0d534def1db2a80405dbde9e780d3f32b00f2f4f
Peter-PM <tangompp@gmail.com>
2021-10-21 20:14:29 +0300

    adaptive header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -12,10 +12,24 @@ function Header() {
           <li className={styles.item}><a href="/" className={styles.link}>Рассчитать кредит</a></li>
           <li className={styles.item}><a href="/" className={styles.link}>Конвертер валют</a></li>
           <li className={styles.item}><a href="/" className={styles.link}>Контакты</a></li>
-          <li className={styles.item}><a href="/" className={styles.link}>Задать вопрос</a></li>
         </ul>
       </nav>
-      <a href="/" className={`${styles.link} ${styles.enter}`}>Войти в Интернет-банк</a>
+      <a href="/" className={`${styles.link} ${styles.enter}`}>
+        <svg className={styles.enter__svg} width="20" height="22">
+          <use xlinkHref="../../img/icons.svg#enter"></use>
+        </svg>
+        <span className={styles.enter__description}>Войти в Интернет-банк</span>
+      </a>
+      <button className={`${styles.button} ${styles.button__menu}`}>
+        <svg className={styles.enter__svg} width="16" height="12">
+          <use xlinkHref="../../img/icons.svg#burger"></use>
+        </svg>
+      </button>
+      <button className={`${styles.button} ${styles.button__close}`}>
+        <svg className={styles.enter__svg} width="15" height="15">
+          <use xlinkHref="../../img/icons.svg#cross"></use>
+        </svg>
+      </button>
     </header>
   );
 }
