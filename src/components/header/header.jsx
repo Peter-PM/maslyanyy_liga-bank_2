bba355d14eef283f3f02b39ef4bd9079a133fda4
Peter-PM <tangompp@gmail.com>
2021-10-21 21:09:24 +0300

    open/close menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,96 @@
-import React from 'react';
-import styles from './header.module.scss';
-import Logo from '../logo/logo';
+import React, { useState } from "react";
+import styles from "./header.module.scss";
+import Logo from "../logo/logo";
 
 function Header() {
+  const [menuState, setMenuState] = useState(true);
+
+  const handleResize = () => {
+    if (window.innerWidth > 767) {
+      setMenuState(true);
+      window.removeEventListener("resize", handleResize);
+      console.log('up')
+    }
+  }
+
+  window.addEventListener("resize", handleResize);
+  
   return (
     <header className={styles.header}>
-      <Logo/>
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li className={styles.item}><a href="/" className={styles.link}>Услуги</a></li>
-          <li className={styles.item}><a href="/" className={styles.link}>Рассчитать кредит</a></li>
-          <li className={styles.item}><a href="/" className={styles.link}>Конвертер валют</a></li>
-          <li className={styles.item}><a href="/" className={styles.link}>Контакты</a></li>
-        </ul>
-      </nav>
-      <a href="/" className={`${styles.link} ${styles.enter}`}>
-        <svg className={styles.enter__svg} width="20" height="22">
-          <use xlinkHref="../../img/icons.svg#enter"></use>
-        </svg>
-        <span className={styles.enter__description}>Войти в Интернет-банк</span>
-      </a>
-      <button className={`${styles.button} ${styles.button__menu}`}>
-        <svg className={styles.enter__svg} width="16" height="12">
-          <use xlinkHref="../../img/icons.svg#burger"></use>
-        </svg>
-      </button>
-      <button className={`${styles.button} ${styles.button__close}`}>
-        <svg className={styles.enter__svg} width="15" height="15">
-          <use xlinkHref="../../img/icons.svg#cross"></use>
-        </svg>
-      </button>
+      <Logo />
+      {menuState ? (
+        <>
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <a href="/" className={styles.link}>
+                  Услуги
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href="/" className={styles.link}>
+                  Рассчитать кредит
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href="/" className={styles.link}>
+                  Конвертер валют
+                </a>
+              </li>
+              <li className={styles.item}>
+                <a href="/" className={styles.link}>
+                  Контакты
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <a href="/" className={`${styles.link} ${styles.enter}`}>
+            <svg className={styles.enter__svg} width="20" height="22">
+              <use xlinkHref="../../img/icons.svg#enter"></use>
+            </svg>
+            <span className={styles.enter__description}>
+              Войти в Интернет-банк
+            </span>
+          </a>
+          <button
+            className={`${styles.button} ${styles.button__menu}`}
+            onClick={() => {
+              setMenuState(!menuState);
+            }}
+          >
+            <svg className={styles.enter__svg} width="16" height="12">
+              <use xlinkHref="../../img/icons.svg#burger"></use>
+            </svg>
+          </button>
+          <button
+            className={`${styles.button} ${styles.button__close}`}
+            onClick={() => setMenuState(false)}
+          >
+            <svg className={styles.enter__svg} width="15" height="15">
+              <use xlinkHref="../../img/icons.svg#cross"></use>
+            </svg>
+          </button>
+        </>
+      ) : (
+        <>
+          <a href="/" className={`${styles.link} ${styles.enter__mobile}`}>
+            <svg className={styles.svg__mobile} width="14" height="16">
+              <use xlinkHref="../../img/icons.svg#enter"></use>
+            </svg>
+            <span className={styles.enter__none}>Войти в Интернет-банк</span>
+          </a>
+          <button
+            className={`${styles.button} ${styles.button__menu}`}
+            onClick={() => {
+              setMenuState(!menuState);
+            }}
+          >
+            <svg className={styles.enter__svg} width="16" height="12">
+              <use xlinkHref="../../img/icons.svg#burger"></use>
+            </svg>
+          </button>
+        </>
+      )}
     </header>
   );
 }
